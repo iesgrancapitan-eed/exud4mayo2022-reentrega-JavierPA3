4d22faf86a6aa41e664e614ca4fc5712615dd62a
Javier Postigo Arévalo <a21poarja@iesgrancapitan.org>
2022-05-22 18:12:02 +0200

    refactor: mensaje de la excepción

--- src/com/jpa/eed/examen/optimizacionydocumentacion/EdadErroneaException.java
+++ src/com/jpa/eed/examen/optimizacionydocumentacion/EdadErroneaException.java
@@ -2,7 +2,7 @@ package com.jpa.eed.examen.optimizacionydocumentacion;
 
 public class EdadErroneaException extends Exception {
 
-	public EdadErroneaException(String string) {
-		super(string);
+	public EdadErroneaException(String message) {
+		super(message);
 	}
 }
